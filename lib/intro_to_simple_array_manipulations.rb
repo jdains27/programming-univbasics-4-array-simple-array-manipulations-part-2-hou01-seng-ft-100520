43d4968ebf7b31f4c0c86958b4a4aca11e11afb0
Jordan Dains <dains.jordan2@gmail.com>
2020-09-10 13:44:31 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -2,17 +2,22 @@ def using_concat(array, array2)
   array.concat(array2)
 end 
 
-def using_insert
+def using_insert(array, element)
+  array.insert()
 end 
 
-def using_uniq
+def using_uniq(array)
+  array.uniq 
 end 
 
-def using_flatten
+def using_flatten(array)
+  array.flatten 
 end 
 
-def using_delete
+def using_delete(array, string)
+  array.delete(string)
 end 
 
-def using_delete_at
+def using_delete_at(array, integer)
+  array.delete_at(integer)
 end 
